16fabd6398c20f025068ec6f5bea60fb3a9360ef
Santiago Castellar <santiprocastellar6@gmail.com>
2025-12-16 15:10:14 -0500

    feat: enhance UI components with motion and improve theme toggle
    - Add Framer Motion animations to Button component
    - Improve ThemeToggle with better state management and transitions
    - Add cursor-pointer class to interactive elements
    - Clean up and optimize component code

--- packages/ui/src/atoms/ThemeToggle/ThemeToggle.tsx
+++ packages/ui/src/atoms/ThemeToggle/ThemeToggle.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
@@ -8,16 +10,30 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@ui/atoms/DropdownMenu/dropdown-menu"
+import { useEffect, useState } from "react";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false)
+  const isDark = theme === "dark"
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (!mounted) return null
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="border border-sidebar-border bottom-1 right-2">
-          <Sun className="h-[1.2rem] text-black w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <Button variant="outline" size="icon" className="border border-sidebar-border flex w-7 h-7 flex items-center justify-center rounded cursor-pointer bottom-1 right-2">
+          {isDark ? (
+            <Moon className="h-[1.2rem] w-[1.2rem] transition-all" />
+          ) : (
+            <Sun className="h-[1.2rem] text-black w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          )}
+          {/* <Sun className="hidden h-[1.2rem] text-black w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="hidden h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" /> */}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
